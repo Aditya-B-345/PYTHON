449bfd6ee3532d7049860d650baaafa37ce50bee
Aditya-B-345 <aditya.vithesh345@gmail.com>
2026-07-13 09:37:36 -0700

    Add files via upload

---
+++ multipicaltion_table.py
@@ -0,0 +1,9 @@
+#Multipication of table from 1 up to N
+n=int(input("enter the number n:-"))
+i=1
+while(i<=n):
+    j=1
+    while(j<=10):
+        print(i,"X",j,"=",i*j)
+        j+=1
+    i+=1        
